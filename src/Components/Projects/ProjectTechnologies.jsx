0731211cbe556e6721a88792d3778017a9ee346e
wahabmhar <wahabmhar@gmail.com>
2024-05-22 18:53:13 +0500

    resolve issues

--- src/Components/Projects/ProjectTechnologies.jsx
+++ src/Components/Projects/ProjectTechnologies.jsx
@@ -1,12 +1,54 @@
 import { Box, Container } from "@mui/material";
-import React from "react";
-import TextSphere from "../TextSphere/TextSphere";
+import React, { useEffect } from "react";
+import "./projects.css";
+
+import TagCloud from "TagCloud";
+const container = ".tagcloud";
+const texts = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "ES6",
+    "ReactJS",
+    "NextJS",
+    "Redux",
+    "RTK",
+    "Axios",
+    "TailWind CSS",
+    "Material UI",
+    "WordPress",
+    "NodeJS",
+    "ExpressJS",
+    "Solidity",
+    "Truffle",
+    "Babel",
+    "OneSignal",
+    "Brevo",
+    "EmailJs",
+    "Google Maps",
+    "GIT",
+    "GITHUB",
+];
+
+const options = {
+    radius: 300,
+    maxSpeed: "fast",
+    initSpeed: "fast",
+    keep: true,
+};
 
 const ProjectTechnologies = () => {
+    useEffect(() => {
+        // return () => {
+        TagCloud(container, texts, options);
+        // };
+    }, []);
     return (
         <Box sx={{ bgcolor: "primary.main", display: { xs: "none", sm: "block" } }}>
             <Container sx={{ alignItems: "center", justifyContent: "center", padding: "3rem" }}>
-                <TextSphere />
+                <div className="text-shpere">
+                    <span className="tagcloud"></span>
+                </div>
             </Container>
         </Box>
     );
--- src/Components/TextSphere/TextSphere.js
+++ src/Components/TextSphere/TextSphere.js
@@ -6,53 +6,8 @@ import TagCloud from "TagCloud";
 
 const TextSphere = () => {
     // Animation settings for Text Cloud
-    useEffect(() => {
-        return () => {
-            const container = ".tagcloud";
-            const texts = [
-                "HTML",
-                "CSS",
-                "JavaScript",
-                "ES6",
-                "ReactJS",
-                "NextJS",
-                "Redux",
-                "RTK",
-                "Axios",
-                "TailWind CSS",
-                "Material UI",
-                "WordPress",
-                "NodeJS",
-                "ExpressJS",
-                "Solidity",
-                "Truffle",
-                "Babel",
-                "OneSignal",
-                "Brevo",
-                "EmailJs",
-                "Google Maps",
-                "GIT",
-                "GITHUB",
-            ];
 
-            const options = {
-                radius: 300,
-                maxSpeed: "fast",
-                initSpeed: "fast",
-                keep: true,
-            };
-
-            TagCloud(container, texts, options);
-        };
-    }, []);
-
-    return (
-        <>
-            <div className="text-shpere">
-                <span className="tagcloud"></span>
-            </div>
-        </>
-    );
+    return <></>;
 };
 
 export default TextSphere;
